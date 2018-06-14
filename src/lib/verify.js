2d49bcb25c9f9b665fb0d69220678e1fe9502aa3
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2018-06-14 13:48:54 +0200

    refactor: json manifest file

--- src/lib/verify.js
+++ src/lib/verify.js
@@ -1,33 +1,36 @@
+import fs from 'fs-extra'
+import path from 'path'
 import hasha from 'hasha'
 import log from 'npmlog'
 import lnd from '../lib/extensions'
-import manifest from './manifest'
 import * as pkg from '../../package.json'
 import createDebug from 'debug'
 
 const debug = createDebug(pkg.name)
 
 // Verify the binary archive.
-export const verify = path => {
-  debug('verify: %o', { path })
+export const verify = filepath => {
+  debug('verify: %o', { filepath })
 
   function getKeyByValue(object, value) {
     return Object.keys(object).find(key => object[key] === value)
   }
 
+  const manifestPath = path.join(__dirname, '..', '..', 'config', 'manifest.json')
+  const manifest = fs.readJsonSync(manifestPath)
   const checksums = manifest[lnd.getBinaryVersion()]
   const checksum = getKeyByValue(checksums, lnd.getBinaryName() + lnd.getBinaryExtension())
 
   debug('Verifying archive against checksum', checksum)
 
   return hasha
-    .fromFile(path, { algorithm: 'sha256' })
+    .fromFile(filepath, { algorithm: 'sha256' })
     .then(hash => {
       debug('Generated hash from downloaded file', hash)
 
       if (checksum === hash) {
         log.info(pkg.name, 'Verified checksum of downloaded file')
-        return path
+        return filepath
       }
       log.error(pkg.name, 'Checksum did not match')
       return Promise.reject(new Error('Checksum did not match'))
