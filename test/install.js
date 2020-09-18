9ccc19b9554f3b8d73a3c6293951ebfec5824d28
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2020-09-18 11:17:57 +0100

    feat: add support for 0.11.0-beta

--- test/install.js
+++ test/install.js
@@ -7,9 +7,9 @@ import goenv from 'go-platform'
 import { install } from '../src/lib/install'
 
 const dir = path.resolve(__dirname, '..', 'vendor')
-const VERSION = 'v0.10.1-beta'
+const VERSION = 'v0.11.0-beta'
 
-test('Ensure lnd gets downloaded (current version and platform)', t => {
+test('Ensure lnd gets downloaded (current version and platform)', (t) => {
   t.plan(4)
   fs.removeSync(dir)
 
@@ -19,25 +19,25 @@ test('Ensure lnd gets downloaded (current version and platform)', t => {
   return (
     install()
       // Check return values.
-      .then(res => {
+      .then((res) => {
         t.ok(res.fileName.indexOf(`lnd-${platform}-${arch}-${VERSION}`) !== -1, 'Returns the correct filename')
         t.ok(res.installPath === path.resolve(path.join(__dirname, '..', 'vendor')), 'Returns the correct output path')
       })
 
       // Check download dir.
       .then(() => fs.stat(dir))
-      .then(stats => t.ok(stats, 'lnd was downloaded'))
+      .then((stats) => t.ok(stats, 'lnd was downloaded'))
 
       // Check binary.
       .then(() => fs.stat(path.join(dir, 'lnd')))
-      .then(stats => t.ok(stats, `binary was downloaded`))
+      .then((stats) => t.ok(stats, `binary was downloaded`))
 
       // Cleanup.
       .finally(() => delete process.env.LND_BINARY_PLATFORM)
   )
 })
 
-test('Ensure Windows version gets downloaded', t => {
+test('Ensure Windows version gets downloaded', (t) => {
   t.plan(4)
   fs.removeSync(dir)
 
@@ -48,25 +48,25 @@ test('Ensure Windows version gets downloaded', t => {
   return (
     install()
       // Check return values.
-      .then(res => {
+      .then((res) => {
         t.ok(res.fileName.indexOf(`lnd-${platform}-${arch}-${VERSION}`) !== -1, 'Returns the correct filename')
         t.ok(res.installPath === path.resolve(path.join(__dirname, '..', 'vendor')), 'Returns the correct output path')
       })
 
       // Check download dir.
       .then(() => fs.stat(dir))
-      .then(stats => t.ok(stats, 'lnd was downloaded'))
+      .then((stats) => t.ok(stats, 'lnd was downloaded'))
 
       // Check binary.
       .then(() => fs.stat(path.join(dir, 'lnd.exe')))
-      .then(stats => t.ok(stats, `${platform} binary was downloaded`))
+      .then((stats) => t.ok(stats, `${platform} binary was downloaded`))
 
       // Cleanup.
       .finally(() => delete process.env.LND_BINARY_PLATFORM)
   )
 })
 
-test('Ensure Linux version gets downloaded', t => {
+test('Ensure Linux version gets downloaded', (t) => {
   t.plan(4)
   fs.removeSync(dir)
 
@@ -77,25 +77,25 @@ test('Ensure Linux version gets downloaded', t => {
   return (
     install()
       // Check return values.
-      .then(res => {
+      .then((res) => {
         t.ok(res.fileName.indexOf(`lnd-${platform}-${arch}-${VERSION}`) !== -1, 'Returns the correct filename')
         t.ok(res.installPath === path.resolve(path.join(__dirname, '..', 'vendor')), 'Returns the correct output path')
       })
 
       // Check download dir.
       .then(() => fs.stat(dir))
-      .then(stats => t.ok(stats, 'lnd was downloaded'))
+      .then((stats) => t.ok(stats, 'lnd was downloaded'))
 
       // Check binary.
       .then(() => fs.stat(path.join(dir, 'lnd')))
-      .then(stats => t.ok(stats, `${platform} binary was downloaded`))
+      .then((stats) => t.ok(stats, `${platform} binary was downloaded`))
 
       // Cleanup.
       .finally(() => delete process.env.LND_BINARY_PLATFORM)
   )
 })
 
-test('Ensure OSX version gets downloaded', t => {
+test('Ensure OSX version gets downloaded', (t) => {
   t.plan(4)
   fs.removeSync(dir)
 
@@ -106,18 +106,18 @@ test('Ensure OSX version gets downloaded', t => {
   return (
     install()
       // Check return values.
-      .then(res => {
+      .then((res) => {
         t.ok(res.fileName.indexOf(`lnd-${platform}-${arch}-${VERSION}`) !== -1, 'Returns the correct filename')
         t.ok(res.installPath === path.resolve(path.join(__dirname, '..', 'vendor')), 'Returns the correct output path')
       })
 
       // Check download dir.
       .then(() => fs.stat(dir))
-      .then(stats => t.ok(stats, 'lnd was downloaded'))
+      .then((stats) => t.ok(stats, 'lnd was downloaded'))
 
       // Check binary.
       .then(() => fs.stat(path.join(dir, 'lnd')))
-      .then(stats => t.ok(stats, `${platform} binary was downloaded`))
+      .then((stats) => t.ok(stats, `${platform} binary was downloaded`))
 
       // Cleanup.
       .finally(() => delete process.env.LND_BINARY_PLATFORM)
